18cf4d3d7bb07e3bd9ab944d28648fc3231bae1c
Matheus <matheus.malmeida7@gmail.com>
2022-01-20 18:11:02 -0300

    Criando Model

---
+++ src/app/model/Produtos.ts
@@ -0,0 +1,11 @@
+import { Usuario } from "./Usuario";
+
+export class Produtos{
+    public id: number;
+    public nome: string;
+    public fornecedor: string;
+    public valor: number;
+    public data: Date;
+    public usuario: Usuario;
+
+}
