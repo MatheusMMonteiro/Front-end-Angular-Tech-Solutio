05810ea4f5ea462824a81fe9730fb510e6947310
Matheus <matheus.malmeida7@gmail.com>
2022-01-20 18:12:34 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { CadastrarProdutoComponent } from './produto/cadastrar-produto/cadastrar-produto.component';
+import { DeletarProdutoComponent } from './produto/deletar-produto/deletar-produto.component';
+import { EditarProdutoComponent } from './produto/editar-produto/editar-produto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'cadastro', component: CadastroComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'cadastrar-produto', component: CadastrarProdutoComponent},
+  {path: 'deletar-produto/:id', component: DeletarProdutoComponent},
+  {path: 'editar-produto/:id', component: EditarProdutoComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { CadastrarProdutoComponent } from './produto/cadastrar-produto/cadastrar-produto.component';
+import { EditarProdutoComponent } from './produto/editar-produto/editar-produto.component';
+import { DeletarProdutoComponent } from './produto/deletar-produto/deletar-produto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    InicioComponent,
+    LoginComponent,
+    CadastroComponent,
+    CadastrarProdutoComponent,
+    EditarProdutoComponent,
+    DeletarProdutoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
